61fdcf554d5c4f9a0dd4831e7e0f55a477f1e388
Rodrigo Sandoval <rsandovuaem@gmail.com>
2015-11-08 16:38:49 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,9 @@
 ### Proyecto 1. Exploratory Data Analysis
 ## Rodrigo Sandoval ALmazan
 
-d = read.table("household_power_consumption.txt")  ## para leer el archivo
+d = read.table("household_power_consumption.txt")  ## read the file
+library(dplyr)  ### Open Dplyr
+tbl_df(d)  ## transform in data frame
+table1  <- select(d, contains("2007-02-01"))
+table 2  <- select(d, contains("2007-02-02"))
+table  <- table1 + table2  ## create the work dataframe
