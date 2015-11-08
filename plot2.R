cbb6e19d5df6db01775667a445d4cbb705cc123a
Rodrigo Sandoval <rsandovuaem@gmail.com>
2015-11-08 16:39:27 -0600

    Code for extracting table
    Table extracted using DPLYR

--- plot2.R
+++ plot2.R
@@ -2,3 +2,8 @@
 ## Rodrigo Sandoval ALmazan
 
 d = read.table("household_power_consumption.txt")  ## para leer el archivo
+library(dplyr)  ### Open Dplyr
+tbl_df(d)  ## transform in data frame
+table1  <- select(d, contains("2007-02-01"))
+table 2  <- select(d, contains("2007-02-02"))
+table  <- table1 + table2  ## create the work dataframe
