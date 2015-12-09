4c3c2fba318c3457372c85c1f93d8f3b289704ab
Rodrigo Sandoval <rsandovuaem@gmail.com>
2015-12-08 19:56:34 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,17 +1,17 @@
 ## Rodrigo Sandoval Almazan EDA-Project
 ##plot3.R
-# Loading and subsetting overall data set
+# Downloading data set 
 dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# plot3.R specifics
+# plot3.R creation
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
+## plot 3. labeling
 png("plot3.png", width=480, height=480)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
